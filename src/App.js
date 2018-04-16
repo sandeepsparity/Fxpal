cc1c5ad4de10495a800a9d937e003fe41728de9b
Sandeep R. Mankala <smankala@fxpal.com>
2018-04-16 13:39:41 -0700

    changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React 12!</h1>
+          <h1 className="App-title">Welcome to React 12!ds</h1>
         </header>
         <p className="App-intro">
           To get started, edit and save to reload.
