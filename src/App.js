37bcc56d6a5f60fb3f2509ef0a970d756df3d61b
Sandeep R. Mankala <smankala@fxpal.com>
2018-04-16 14:33:54 -0700

    added the charts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Barchart from './app/components/Barchart';
+import Linechart from './app/components/Linechart';
+//import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React 12!ds</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit and save to reload.
-        </p>
+      <Linechart/>
+      <Barchart/>
       </div>
     );
   }
