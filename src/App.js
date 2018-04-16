35d17db55b57a8cb6f3280a235f5d418aab2c163
Sandeep R. Mankala <smankala@fxpal.com>
2018-04-16 14:37:21 -0700

    added charts

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Linechart/>
-      <Barchart/>
+        <Linechart/>
+        <Barchart/>
       </div>
     );
   }
